85f9d1f1d82d1366ce5917dc59786aa502b552b7
denizayjs <denizay@std.iyte.edu.tr>
2022-12-11 23:17:46 +0300

    create createRoom and joinRoom method

--- public/js/app.js
+++ public/js/app.js
@@ -51,4 +51,95 @@ window.addEventListener("load", () => {
     localImageEl.hide();
     localVideoEl.show();
   });
+
+  //Update Chat Message
+  const updateChatMessages = () => {
+    const html = chatContentTemplate({ messages });
+    const chatContentEl = $("#chat-content");
+    chatContentEl.html(html);
+    //automatically scroll downwards
+    const scrollHeight = chatContentEl.prop("scrollHeight");
+    chatContentEl.animate({ scrollTop: scrollHeight }, "slow");
+  };
+
+  webrtc.on("message", (data) => {
+    if (data.type === "chat") {
+      const message = data.payload;
+      messages.push(message);
+      updateChatMessages();
+    }
+  });
+
+  //Post Local Message
+  const postMessage = (message) => {
+    const chatMessage = {
+      username,
+      message,
+      postedOn: new Date().toLocaleString("en-GB"),
+    };
+
+    //Send to all peers
+    webrtc.sendToAll("chat", chatMessage);
+
+    //Update messages locally
+    messages.push(chatMessage);
+    $("#post-message").val("");
+    updateChatMessages();
+  };
+
+  //Display Chat Interface
+  const showChatRoom = (room) => {
+    console.log(room);
+    //Hide form
+    formEl.hide();
+    const html = chatTemplate({ room });
+    chatEl.html(html);
+    const postForm = $("form");
+
+    //Post Message Validation Rules
+    postForm.form({
+      message: "empty",
+    });
+    $("#post-btn").on("click", () => {
+      const message = $("#post-message").val();
+      postMessage(message);
+    });
+    $("#post-message").on("keyup", (event) => {
+      if (event.keyCode === 13) {
+        const message = $("#post-message").val();
+        postMessage(message);
+      }
+    });
+  };
+
+  //Register new chat room
+  const createRoom = (roomName) => {
+    console.info(`Creating new room:${roomName}`);
+    webrtc.createRoom(roomName);
+    showChatRoom(roomName);
+    postMessage(`${username} created chatroom`);
+  };
+
+  //Join existing chat room
+  const joinRoom = (roomName) => {
+    console.log(`Joining Room: ${roomName}`);
+    webrtc.joinRoom(roomName);
+    showChatRoom(roomName);
+    postMessage(`${username} joined chatroom`);
+  };
+
+  //Click Handler
+  $(".submit").on("click", (event) => {
+    if (!formEl.form("is valid")) {
+      return false;
+    }
+    username = $("#username").val();
+    const roomName = $("#roomName").val().toLowerCase();
+    if (event.target.id === "create-btn") {
+      createRoom(roomName);
+    } else {
+      joinRoom(roomName);
+    }
+    return false;
+  });
 });
